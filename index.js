a8a6439735ef2497d29783013c59aa15333159af
Alessandro <78975581+AleDellaMorte@users.noreply.github.com>
2022-12-15 12:00:04 +0100

    removed folder

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/*-----Scroll-----*/
 window.addEventListener("scroll", () => {
     let head = document.querySelector(".head");
     let screenPos = window.scrollY;
@@ -8,19 +7,6 @@ window.addEventListener("scroll", () => {
         head.classList.remove("scrolled");
     }
 });
-
-/*-----Checkbox-----*/
-let checkbox = document.getElementById("socials");
-let listCont = document.querySelector(".listCont");
-checkbox.checked = false;
-
-checkbox.addEventListener('change', function() {
-    if (this.checked) {
-        listCont.classList.add("opened");
-    } else {
-        listCont.classList.remove("opened");
-    }
-  });
 
 /*-----KonamiCode-----*/
 var konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
