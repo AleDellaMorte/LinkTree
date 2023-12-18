be8ec4e56ad80a971f9fb8d9918f91e0a380c446
Alessandro <78975581+AleDellaMorte@users.noreply.github.com>
2023-12-18 16:18:56 +0100

    Update index.js
    removed mal api

--- index.js
+++ index.js
@@ -30,9 +30,3 @@ document.addEventListener('keydown', function(e) {
 function easterEgg() {
     window.location.href = "https://youtu.be/1JNmz17gnMw";
 }
-const mal_api = "https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=ce9d6486c30c9e4efc3ac96baccd1d6d&code_challenge=75eGC-9ar46ZQCW0ci98ZAmI0uM5A3AJGa3wPFdE5uyFAy1GLsTokmEdtbYBUX5gD4XMTyoFUC1XZsDWyrxHpcvD6vsQaMKlH3hWiXoh9tbq9XP-C8Em3g1BBep5TdkC&state=RequestID42";
-async function logMovies() {
-    const response = await fetch(mal_api);
-    const movies = await response.json();
-    console.log(movies);
-  }
